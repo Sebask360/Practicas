e82ee5d2b5b7ba9db1de16696ff9545133502456
Sebask360 <sebastiancusism@gmail.com>
2024-07-18 23:04:39 -0500

    Commit 1 practicas y preliminar del programa

---
+++ Practicas/Practica1.py
@@ -0,0 +1,100 @@
+#Practica Print
+
+print('Practicas de Print \n')
+#Practica Print 1
+print("Me encanta estudiar python \n")
+
+#Practica Print 2
+print("Estudiar con \"Python Total\" es super divertido \n")
+
+#Practica Print 3
+print(500 + 50 + 5, "\n")
+
+#Practicaa String
+print('Practicas de String \n')
+
+#Practica String 1
+print("Linea 1" "\n" "Linea 2" "\n" "Linea 3 \n" ) 
+
+#Practica String 2
+print("A\tB\tC\nD\tE\tF\nG\tH\tI \n")
+
+#Practica de Input
+print('Practica de Imput \n')
+
+#Practica Input 1
+estudio = input("Dime que estas estudiando \n")
+
+#Practica Input 2
+pais = input("¿En que pais vives? \n")
+
+print(f"Tu vives en: {pais}")
+print(f"Tu estas estudiando: {estudio}\n")
+
+#Practica Input 3
+nombre = input("Escribe tu nombre: ")
+apellido = input("Escribe tu apellido: ")
+
+print(f"Hola, {nombre} {apellido} \n")
+
+#Practica de Variables 
+print("Praactica de Variables \n")
+
+#Practicas con Variables 1
+
+nombre = "Tony Soprano"
+edad = 51 
+
+#Practicas con variables 2
+
+nombre = "Julia"
+apellido = "Roberts"
+nombrecompleto = ((nombre) + " " + str(apellido))
+print(nombrecompleto + "\n")
+
+#Practicas con Varables 3
+
+curso = "Python" 
+print("Estas tomando un curso de" + " " + str(curso))
+print("Estas tomando un curso de" + " " + curso + "\n")
+
+#Practica con Integers 
+
+print("Practica con Integers")
+num_entero = 7
+print(type(num_entero), "\n")
+
+#Practica con Floats 
+
+print("Practica con Floats")
+
+num_decimal = 3.141592653
+print(type(num_decimal), "\n")
+
+#Practica con Tipos de Datos Numericos 
+print("Practica con Tipos de datos Numericos")
+
+num1 = 7.5
+num2 = 2.5
+resultado = (num1 + num2)
+print(type(resultado))
+print(f"Resultado ", + (resultado), )
+
+# #Practica con Conversiones 1
+
+print("Conversiones")
+
+int(num1)
+print(type(num1), "\n")
+
+#practica de Conversiones 2
+
+float(num2)
+print(type(num2), "\n")
+
+#Practica con Conversiones 3
+
+print(type(num1 + num2), "\n")
+
+
+
---
+++ Practicas/Practica1_Programa_Recoleccion_Datos.py
@@ -0,0 +1,25 @@
+def imprimir_recuadro(texto):
+  lines = texto.split('\n')
+  max_length = max(len(lines) for lines in lines)
+  border = '+' + '-' * (max_length +2) + '+'
+  
+  print(border)
+  for lines in lines:
+    print(f'| {lines.ljust(max_length)} |')
+  print(border)
+  
+nombre = input("Ingrese su nombre: ")
+apellido = input("ingrese su apellido: ")
+edad = input("ingrese su edad: ")
+nombrecompleto = ((nombre)+ " " +str(apellido))
+
+num_uno = input("Ingrese un numero: ")
+num_int = float(num_uno)
+doble_numero = num_int * 2
+
+texto = (
+  f"\nBienvenido {nombrecompleto}, Tienes {edad} años\n\n"\
+  f"Ingresaste {num_int}, y el doble de {num_int} es {doble_numero}\n"
+)
+
+imprimir_recuadro(texto)
